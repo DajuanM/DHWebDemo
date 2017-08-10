babdfaa86efed650cac2f26ee7bc050f345ec292
Aiden123 <252289287@qq.com>
2017-08-10 10:06:55 +0800

    修改

--- 01_DHBannerView/index.js
+++ 01_DHBannerView/index.js
@@ -5,20 +5,21 @@ var count
 var n = 0
 $(document).ready(function () {
 
-    var t = 0
+    var t
     count = $("#content a").length
+
     $("#banner li").click(function () {
         var i = $(this).text() - 1
         n = i
         $("#title-info").html($("#content a").eq(i).find("img").attr("alt"))
         $("#content a").fadeOut(1000).parent().children().eq(i).fadeIn(1000)
+        //toggleClass 有该类名就移除 没有就添加
         $(this).toggleClass("on")
         //siblings("")  获得匹配集合中每个元素的同胞，通过选择器进行筛选是可选的。
         $(this).siblings("").removeAttr("class")
-
     })
+    $("#banner li").eq(0).trigger("click")
     t = setInterval("showAuto()",2000)
-
 })
 
 function showAuto() {
