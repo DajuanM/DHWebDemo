a51913b8d0e55e2e8c0a4a199196552dc386478f
Aiden123 <252289287@qq.com>
2017-08-23 16:58:50 +0800

    no message

---
+++ 03_DHCanvasGame/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by swartz006 on 2017/8/14.
+ */
+
+var canvas = document.createElement("canvas")
+canvas.width = 200
+canvas.height = 200
+var ctx = canvas.getContext("2d")
+ctx.fillStyle = "#ff0000"
+ctx.fillRect(0, 0, 200, 200)
+document.body.appendChild(canvas)
+
